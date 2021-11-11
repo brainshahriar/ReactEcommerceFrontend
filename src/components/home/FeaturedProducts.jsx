394d1b81439d432788eb8b045870ba0e14ed77f9
brain <shahriarmehedi95@gmail.com>
2021-11-11 17:07:48 +0600

    create component

---
+++ src/components/home/FeaturedProducts.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FeaturedProducts extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default FeaturedProducts
