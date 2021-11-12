2d66690443ff21afae38a36fc134e9ee162f17db
brain <shahriarmehedi95@gmail.com>
2021-11-12 13:34:45 +0600

    animate

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/assets/css/custom.css'
-import '../src/assets/css/fontawesome.css'
+import '../src/assets/css/custom.css';
+import '../src/assets/css/fontawesome.css';
+import '../src/assets/css/animate.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
