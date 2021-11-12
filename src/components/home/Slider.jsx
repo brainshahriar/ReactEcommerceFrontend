b4c12be0ae31d9136920f8e6030025885b29e57d
brain <shahriarmehedi95@gmail.com>
2021-11-12 15:06:56 +0600

    slider

--- src/components/home/Slider.jsx
+++ src/components/home/Slider.jsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
- class Slider extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+class Slider extends Component {
+     render() {
+          return (
+               <Fragment>
+                     <h1>Slider</h1>
+               </Fragment>
+          )
+     }
 }
-
 export default Slider
