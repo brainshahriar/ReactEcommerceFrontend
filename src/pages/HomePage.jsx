0421744402f263b7c50ea4af4eec7f44231b0acf
brain <shahriarmehedi95@gmail.com>
2021-11-12 20:15:01 +0600

    navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import NavMenuDekstop from '../components/common/NavMenuDekstop'
 import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
@@ -9,11 +10,12 @@ class HomePage extends Component {
     render() {
         return (
             <Fragment>
+                <NavMenuDekstop />
                 <HomeTop />
                 <FeaturedProducts />
                 <NewArrival />
-                <Collection />
                 <Categories />
+                <Collection />
             </Fragment>
         )
     }
