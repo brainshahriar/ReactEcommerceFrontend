a5194a3a113a62269454cd43eab1b1e6073b0678
brain <shahriarmehedi95@gmail.com>
2021-11-13 17:43:21 +0600

    megamenuall

--- src/components/common/NavMenuMobile.jsx
+++ src/components/common/NavMenuMobile.jsx
@@ -44,8 +44,7 @@ class NavMenuMobile extends Component {
             <Row>
                 <Col lg={4} md={4} sm={12} xs={12}>
 
-                <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button>
-
+                <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button> 
                 <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
 
                 <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 3 Items </Button>
