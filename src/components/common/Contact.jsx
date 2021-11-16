69fcb84f1993e7925d838ac10b694ca0601b2caa
brain <shahriarmehedi95@gmail.com>
2021-11-16 22:06:37 +0600

    contact us

--- src/components/common/Contact.jsx
+++ src/components/common/Contact.jsx
@@ -3,6 +3,35 @@ import { Container,Row,Col, Form,Button } from 'react-bootstrap'
 
 
 export class Contact extends Component {
+     constructor(){
+          super();
+          this.state={
+               name:"",
+               email:"",
+               message:"",
+          }
+     }
+     nameOnChange = (e) =>{
+          let name=e.target.value;
+          //alert(name);
+          this.setState({name:name})
+     }
+     emailOnChange = (e) =>{
+          let email=e.target.value;
+         // alert(email);
+         this.setState({email:email})
+
+     }
+     messageOnChange = (e) =>{
+          let message=e.target.value;
+         // alert(message);
+         this.setState({message:message})
+
+     }
+     onFormSubmit = (e) =>{
+          alert("HEllo");
+          e.preventDefault();
+     }
      render() {
           return (
                <Fragment>
@@ -12,18 +41,18 @@ export class Contact extends Component {
 
                     <Row className="text-center">
              <Col className="d-flex justify-content-center" md={6} lg={6} sm={12} xs={12}>
-     <Form className="onboardForm">
+     <Form className="onboardForm" onSubmit={this.onFormSubmit}>
           <h4 className="section-title-login">CONTACT WITH US </h4>
           <h6 className="section-sub-title">Please Contact With Us </h6>
-          <input className="form-control m-2" type="text" placeholder="Enter Mobile Number" />
+          <input onChange={this.nameOnChange} className="form-control m-2" type="text" placeholder="Enter Your Name" />
 
-          <input className="form-control m-2" type="email" placeholder="Enter Email" />
+          <input onChange={this.emailOnChange} className="form-control m-2" type="email" placeholder="Enter Email" />
 
-          <input className="form-control m-2" type="text" placeholder="Enter Your Message" />
-          <Button className="btn btn-block m-2 site-btn-login"> Send </Button>
+          <Form.Control onChange={this.messageOnChange} className="form-control m-2" as="textarea" rows={3} placeholder="Message" />
+
+          <Button type="submit" className="btn btn-block m-2 site-btn-login"> Send </Button>
 
      </Form>
-
 
                          </Col>
 
@@ -35,11 +64,6 @@ export class Contact extends Component {
            </p>
            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d162771.1102477064!2d-74.10054944459704!3d40.70681480276415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1627241390779!5m2!1sen!2sbd" width="550" height="450" styles="border:0;" allowfullscreen="" loading="lazy"></iframe>                         </Col>
                     </Row>
-
-
-
-
-
 
                          </Col>
                     </Row>
