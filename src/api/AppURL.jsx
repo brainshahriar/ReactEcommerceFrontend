54a062b7d37c821e87082ed079253b938d561efb
brain <shahriarmehedi95@gmail.com>
2021-11-17 21:35:02 +0600

    about us

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -3,6 +3,8 @@ class AppURL {
     static BaseURL="http://localhost:8000/api"
     static VisitorDetails = this.BaseURL+"/getvisitor"
     static PostContact = this.BaseURL+"/postcontact"
+    static AllSiteInfo = this.BaseURL+"/allsiteinfo"
+
 }
 
 export default AppURL
