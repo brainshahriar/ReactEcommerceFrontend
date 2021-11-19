3d71690165656bb6f4023ae2aa9acad2e552eba4
brain <shahriarmehedi95@gmail.com>
2021-11-19 22:02:05 +0600

    placeholderslider

--- src/components/home/HomeTop.jsx
+++ src/components/home/HomeTop.jsx
@@ -4,13 +4,16 @@ import MegaMenu from './MegaMenu'
 import HomeSlider from './HomeSlider'
 import AppURL from '../../api/AppURL';
 import axios from 'axios'
+import SliderLoading from '../PlaceHolder/SliderLoading';
 
 class HomeTop extends Component {
      constructor(){
           super();
           this.state ={
                MenuData:[],
-               SliderData:[]
+               SliderData:[],
+               isLoading:"",
+               mainDiv:"d-none"
           }
      }
 
@@ -23,7 +26,8 @@ class HomeTop extends Component {
           });
 
           axios.get(AppURL.AllSlider).then(response =>{ 
-               this.setState({SliderData:response.data});
+               this.setState({SliderData:response.data,isLoading:"d-none",
+               mainDiv:""});
 
          }).catch(error=>{
 
@@ -33,6 +37,9 @@ class HomeTop extends Component {
      render() {
           return (
                <Fragment>
+               <SliderLoading isLoading={this.state.isLoading} />
+
+               <div className={this.state.mainDiv}>
         <Container className="p-0 m-0 overflow-hidden" fluid={true}>
                          <Row>
                               <Col lg={3} md={3} sm={12}>
@@ -46,6 +53,7 @@ class HomeTop extends Component {
                               </Col>
                          </Row>
                     </Container>
+                    </div>
                </Fragment>
           )
      }
