450a9d66c7987475d6da78760e8dcffbd65614c4
brain <shahriarmehedi95@gmail.com>
2021-11-22 22:47:06 +0600

    breadcrumb

--- src/components/others/Purchase.jsx
+++ src/components/others/Purchase.jsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 import parse from 'html-react-parser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import { Link } from 'react-router-dom';
 
 export class Purchase extends Component {
      constructor(){
@@ -47,6 +49,13 @@ export class Purchase extends Component {
           return (
                <Fragment>
                <Container>
+                    
+               <div className="breadbody">
+               <Breadcrumb>
+  <Breadcrumb.Item> <Link to="/"> Home </Link> </Breadcrumb.Item>
+  <Breadcrumb.Item> <Link to="/purchase"> Purchase </Link> </Breadcrumb.Item>   
+</Breadcrumb>
+</div>
                     <Row className="p-2">
             <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
 <div className={this.state.loaderDiv}>
--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -1,46 +1,58 @@
 import React, { Component, Fragment } from 'react'
-import { Router,Route,Switch } from 'react-router'
-import UserLoginPage from "../pages/UserLoginPage";
-import HomePage from '../pages/HomePage'
-import ProductDetailsPage from '../pages/ProductDetailsPage';
+import { Router, Route, Switch } from "react-router";
+import AboutPage from '../pages/AboutPage';
+import CartPage from '../pages/CartPage';
 import ContactPage from '../pages/ContactPage';
+import FavouritePage from '../pages/FavouritePage';
+import HomePage from '../pages/HomePage';
+import NotificationPage from '../pages/NotificationPage';
 import PrivacyPage from '../pages/PrivacyPage';
+import ProductCategoryPage from '../pages/ProductCategoryPage';
+import ProductDetailsPage from '../pages/ProductDetailsPage';
+import ProductSubCategoryPage from '../pages/ProductSubCategoryPage';
 import PurchasePage from '../pages/PurchasePage';
 import RefundPage from '../pages/RefundPage';
-import NotificationPage from '../pages/NotificationPage';
-import FavouritePage from '../pages/FavouritePage';
-import CartPage from '../pages/CartPage';
-import AboutPage from '../pages/AboutPage';
-import ProductCategoryPage from '../pages/ProductCategoryPage';
-import ProductSubCategoryPage from '../pages/ProductSubCategoryPage';
+import UserLoginPage from '../pages/UserLoginPage';
 
 class AppRoute extends Component {
-    render() {
-        return (
-            <Fragment>
-            <Switch>
-                 <Route exact path="/" component={HomePage} />
+     render() {
+          return (
+     <Fragment>
+          <Switch>
+               
 
-                 <Route exact path="/" render={(props)=> <HomePage {...props}key={Date.now()}/>} />
+ <Route exact path="/" render={(props) => <HomePage {...props} key={Date.now()} /> } />
 
+ <Route exact path="/login" render={(props) => <UserLoginPage {...props} key={Date.now()} /> } />
+ 
+ <Route exact path="/contact" render={(props) => <ContactPage {...props} key={Date.now()} /> } />
 
-                 <Route exact path="/login" component={UserLoginPage} />
-                 <Route exact path="/contact" component={ContactPage} />
+ <Route exact path="/purchase" render={(props) => <PurchasePage {...props} key={Date.now()} /> } />
 
-                 <Route exact path="/purchase" component={PurchasePage} />
-               <Route exact path="/privacy" component={PrivacyPage} />
-               <Route exact path="/refund" component={RefundPage} />
-               <Route exact path="/about" component={AboutPage} />
-               <Route exact path="/productdetails/:code" component={ProductDetailsPage} />
-               <Route exact path="/notification" component={NotificationPage} />
-               <Route exact path="/favourite" component={FavouritePage} />
-               <Route exact path="/cart" component={CartPage} />
-               <Route exact path="/productcategory/:category" component={ProductCategoryPage} />
-               <Route exact path="/productsubcategory/:category/:subcategory" component={ProductSubCategoryPage} />
-            </Switch>
-            </Fragment>
-        )
-    }
+ <Route exact path="/privacy" render={(props) => <PrivacyPage {...props} key={Date.now()} /> } /> 
+
+ <Route exact path="/refund" render={(props) => <RefundPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/about" render={(props) => <AboutPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/productdetails/:code" render={(props) => <ProductDetailsPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/notification" render={(props) => <NotificationPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/favourite" render={(props) => <FavouritePage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/cart" render={(props) => <CartPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/productcategory/:category" render={(props) => <ProductCategoryPage {...props} key={Date.now()} /> } />
+
+ <Route exact path="/productsubcategory/:category/:subcategory" render={(props) => <ProductSubCategoryPage {...props} key={Date.now()} /> } /> 
+              
+
+          </Switch>
+
+     </Fragment>
+          )
+     }
 }
 
 export default AppRoute
