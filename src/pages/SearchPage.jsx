4bb7b4a73cfa77afbf48fdb7f6227746eb14bf6c
brain <shahriarmehedi95@gmail.com>
2021-11-23 21:15:35 +0600

    search key area

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,13 +1,60 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import { Fragment } from 'react'
+import AppURL from '../api/AppURL'
+import FooterDekstop from '../components/common/FooterDekstop'
+import FooterMobile from '../components/common/FooterMobile'
+import NavMenuDekstop from '../components/common/NavMenuDekstop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
+import axios from 'axios'
+import SearchList from '../components/ProductDetails/SearchList'
 
 class SearchPage extends Component {
-    render() {
-        return (
-            <Fragment>
-                <h1>search page</h1>
-            </Fragment>
-        )
-    }
+
+     constructor({match}){
+          super(); 
+          this.state={
+               SearchKey:match.params.searchkey,
+               ProductData:[] 
+          }
+     }
+
+     componentDidMount(){
+          window.scroll(0,0)
+          // alert(this.state.Category);
+          axios.get(AppURL.ProductBySearch(this.state.SearchKey)).then(response =>{
+               
+               this.setState({ProductData:response.data});         
+
+          }).catch(error=>{
+
+          });
+
+     } 
+
+     render() {
+          return (
+               <Fragment> 
+               <div className="Desktop">
+                <NavMenuDekstop /> 
+               </div>
+
+               <div className="Mobile">
+               <NavMenuMobile />  
+               </div>                       
+
+               <SearchList SearchKey={this.state.SearchKey} ProductData={this.state.ProductData} /> 
+               
+               <div className="Desktop">
+               <FooterDekstop/>
+               </div>
+
+               <div className="Mobile">
+               <FooterMobile/>
+               </div>
+               
+          </Fragment>
+          )
+     }
 }
 
 export default SearchPage
